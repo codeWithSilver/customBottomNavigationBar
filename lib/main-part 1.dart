41345ee814c3e94de051f576863b61c2ed317aff
codeWithSilver <61716921+codeWithSilver@users.noreply.github.com>
2020-05-08 09:11:25 -0400

    main -> part 2 and main-part 1

---
+++ lib/main-part 1.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FinantialApp());
+}
+
+class FinantialApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Finantial App",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedItemIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Row(
+        children: <Widget>[
+          buildNavBarItem(Icons.home, 0),
+          buildNavBarItem(Icons.card_giftcard, 1),
+          buildNavBarItem(Icons.camera, 2),
+          buildNavBarItem(Icons.pie_chart, 3),
+          buildNavBarItem(Icons.person, 4),
+        ],
+      ),
+      appBar: AppBar(
+        title: Text("Custom Bottom Navigation Bar"),
+      ),
+      body: Center(
+        child: Text("Hello from Item $_selectedItemIndex"),
+      ),
+    );
+  }
+
+  Widget buildNavBarItem(IconData icon, int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedItemIndex = index;
+        });
+      },
+      child: Container(
+        height: 60,
+        width: MediaQuery.of(context).size.width / 5,
+        decoration: index == _selectedItemIndex
+            ? BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(width: 4, color: Colors.green),
+                ),
+                gradient: LinearGradient(colors: [
+                  Colors.green.withOpacity(0.3),
+                  Colors.green.withOpacity(0.015),
+                ], begin: Alignment.bottomCenter, end: Alignment.topCenter)
+                // color: index == _selectedItemIndex ? Colors.green : Colors.white,
+                )
+            : BoxDecoration(),
+        child: Icon(
+          icon,
+          color: index == _selectedItemIndex ? Colors.black : Colors.grey,
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -20,40 +20,92 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _selectedItemIndex = 0;
+  int _selectedItem = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: Row(
-        children: <Widget>[
-          buildNavBarItem(Icons.home, 0),
-          buildNavBarItem(Icons.card_giftcard, 1),
-          buildNavBarItem(Icons.camera, 2),
-          buildNavBarItem(Icons.pie_chart, 3),
-          buildNavBarItem(Icons.person, 4),
+      bottomNavigationBar: CustomBottomNavigationBar(
+        iconList: [
+          Icons.home,
+          Icons.card_giftcard,
+          Icons.camera,
+          Icons.pie_chart,
+          Icons.person,
         ],
+        onChange: (val) {
+          setState(() {
+            _selectedItem = val;
+          });
+        },
+        defaultSelectedIndex: 1,
       ),
       appBar: AppBar(
-        title: Text("Custom Bottom Navigation Bar"),
+        title: Text("Home"),
       ),
       body: Center(
-        child: Text("Hello from Item $_selectedItemIndex"),
+        child: Text(
+          "Hello from Item $_selectedItem",
+          style: TextStyle(fontSize: 26),
+        ),
       ),
+    );
+  }
+}
+
+class CustomBottomNavigationBar extends StatefulWidget {
+  final int defaultSelectedIndex;
+  final Function(int) onChange;
+  final List<IconData> iconList;
+
+  CustomBottomNavigationBar(
+      {this.defaultSelectedIndex = 0,
+      @required this.iconList,
+      @required this.onChange});
+
+  @override
+  _CustomBottomNavigationBarState createState() =>
+      _CustomBottomNavigationBarState();
+}
+
+class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
+  int _selectedIndex = 0;
+  List<IconData> _iconList = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _selectedIndex = widget.defaultSelectedIndex;
+    _iconList = widget.iconList;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> _navBarItemList = [];
+
+    for (var i = 0; i < _iconList.length; i++) {
+      _navBarItemList.add(buildNavBarItem(_iconList[i], i));
+    }
+
+    return Row(
+      children: _navBarItemList,
     );
   }
 
   Widget buildNavBarItem(IconData icon, int index) {
     return GestureDetector(
       onTap: () {
+        widget.onChange(index);
         setState(() {
-          _selectedItemIndex = index;
+          _selectedIndex = index;
         });
       },
       child: Container(
         height: 60,
-        width: MediaQuery.of(context).size.width / 5,
-        decoration: index == _selectedItemIndex
+        width: MediaQuery.of(context).size.width / _iconList.length,
+        decoration: index == _selectedIndex
             ? BoxDecoration(
                 border: Border(
                   bottom: BorderSide(width: 4, color: Colors.green),
@@ -67,7 +119,7 @@ class _HomePageState extends State<HomePage> {
             : BoxDecoration(),
         child: Icon(
           icon,
-          color: index == _selectedItemIndex ? Colors.black : Colors.grey,
+          color: index == _selectedIndex ? Colors.black : Colors.grey,
         ),
       ),
     );
